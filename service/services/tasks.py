b92b1e72733845e2338da56f80a13b82e1983086
VladlmlrF <vladimirfalin84@gmail.com>
2023-10-05 16:55:14 +0700

    created cache

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,8 +1,10 @@
 import datetime
 
 from celery import shared_task
+from django.conf import settings
 from django.db import transaction
 from django.db.models import F
+from django.core.cache import cache
 
 
 @shared_task
@@ -16,6 +18,7 @@ def set_price(subscription_id):
                             F('service__full_price') * F('plan__discount_percent') / 100.00).first()
         subscription.price = subscription.annotated_price
         subscription.save()
+    cache.delet(settings.PRICE_CACHE_NAME)
 
 
 @shared_task
@@ -26,3 +29,4 @@ def set_comment(subscription_id):
         subscription = Subscription.objects.select_for_update().get(id=subscription_id)
         subscription.comment = str(datetime.datetime.now())
         subscription.save()
+    cache.delet(settings.PRICE_CACHE_NAME)
